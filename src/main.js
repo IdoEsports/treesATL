0cfd9b468a5f28eb31fd91ee2b48bf2ad638f471
Abhijeet Surakanti <abhijeetsurakanti@gmail.com>
2024-08-09 01:49:07 -0400

    Minimum Map with api key set as env variable

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import "/style.css";
+import mapboxgl from "mapbox-gl";
 
-document.querySelector("#app").innerHTML = `
-	<div>Hello World</div>
-`;
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+const map = new mapboxgl.Map({
+	container: "map", // container ID
+	style: "mapbox://styles/mapbox/streets-v12", // style URL
+	center: [-84.39, 33.75], // starting position [lng, lat]
+	zoom: 9, // starting zoom
+});
