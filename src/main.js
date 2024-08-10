b2039e6d90ba87083a9eb7b01cfe597c437f56c7
Abhijeet Surakanti <abhijeetsurakanti@gmail.com>
2024-08-10 03:04:51 -0400

    Add: Unclean Spatial Data (for markers)
    Wrote python script to download required spatial data from Atlanta
    Trees. It's still unclean, but once clean it should be ready to be put
    in as markers.

--- src/main.js
+++ src/main.js
@@ -5,5 +5,5 @@ const map = new mapboxgl.Map({
 	container: "map", // container ID
 	style: "mapbox://styles/mapbox/streets-v12", // style URL
 	center: [-84.39, 33.75], // starting position [lng, lat]
-	zoom: 9, // starting zoom
+	zoom: 10, // starting zoom
 });
