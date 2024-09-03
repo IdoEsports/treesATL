794ef2019fc5746f303a9f07e74f9371dd423c07
Abhijeet Surakanti <abhijeetsurakanti@gmail.com>
2024-09-02 21:32:01 -0400

    Create: Main Style in MapBox Studio Modify: Map uses this style now.

--- src/main.js
+++ src/main.js
@@ -3,24 +3,30 @@ import mapboxgl from "mapbox-gl";
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 const map = new mapboxgl.Map({
 	container: "map", // container ID
+	style: "mapbox://styles/aesbetic/cm0lb503k025v01qrfdorcejd/draft",
 	center: [-84.39, 33.75], // starting position [lng, lat]
-	zoom: 10, // starting zoom
+	zoom: 11.8, // starting zoom
+	pitch: 55,
 });
 
-map.on("load", () => {
-	map.addSource("atlTrees", {
-		type: "vector",
-		url: "mapbox://aesbetic.Atlanta_Trees",
-	});
+/*
+	TODO: Add popups
+*/
 
-	map.addLayer({
-		id: "trees",
-		type: "circle",
-		source: "atlTrees",
-		"source-layer": "Atlanta_Trees",
-	});
-
-	map.on("style.load", () => {
-		map.setConfigProperty("basemap", "show3dObjects", true);
-	});
-});
+// map.on("load", () => {
+// 	map.addSource("atlTrees", {
+// 		type: "vector",
+// 		url: "mapbox://aesbetic.Atlanta_Trees",
+// 	});
+//
+// 	map.addLayer({
+// 		id: "trees",
+// 		type: "circle",
+// 		source: "atlTrees",
+// 		"source-layer": "Atlanta_Trees",
+// 	});
+//
+// 	map.on("style.load", () => {
+// 		map.setConfigProperty("basemap", "show3dObjects", true);
+// 	});
+// });
